d1b5c5cb3489291c21b98914a1047c2c19ac71f9
CRSPN <unconfigured@null.spigotmc.org>
2020-07-09 13:44:22 -0600

    added npm to use the inquirer packages functionality

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
 
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
 
 
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+
+
+// const generatePage = require('./src/page-template.js');
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// const [name, github] = profileDataArgs;
+
+
+
+
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
